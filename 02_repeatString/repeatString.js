0b45c0b02c6606a6c682d985652129a2b2e93b12
paulauskas-linas <paulauskas.linas@gmail.com>
2022-03-23 07:01:53 +0200

    02 exercise done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let newString = '';
+    if (num<0) {
+        return 'ERROR';
+    } else { 
+        for (let i=0; i<num; i++){
+            newString += string;
+    }
+    return newString;
+}   
+}    
 
 // Do not edit below this line
 module.exports = repeatString;
