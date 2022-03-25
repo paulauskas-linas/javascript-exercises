81bb4aa47880f729192b9fe03588d03e415d3787
paulauskas-linas <paulauskas.linas@gmail.com>
2022-03-25 06:51:20 +0200

    03 exercise done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+const stringArray = string.split('');
+const reversedArray = stringArray.reverse();
+const joinedArray = reversedArray.join('');
+return joinedArray;
 };
 
 // Do not edit below this line
