3e0b9faf7f483b9fe9b343df15e346ba7e3b7b69
paulauskas-linas <paulauskas.linas@gmail.com>
2022-03-29 07:18:36 +0300

    04 exercise done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...arrayToRemove) {
+console.log(originalArray);
+console.log(...arrayToRemove);
+console.log(Array.isArray(arrayToRemove));
+for (let i=0; i<originalArray.length; i++) {
+    for (let j=0; j<arrayToRemove.length; j++) {
+        if (originalArray[i]===arrayToRemove[j]) {
+            originalArray.splice(i,1);
+            i--;
+        } 
+    }
+}
 
+console.log(originalArray);
+return originalArray;
 };
 
 // Do not edit below this line
