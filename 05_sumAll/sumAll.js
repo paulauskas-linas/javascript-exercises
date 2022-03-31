d3d733573c72b39cb0cb02bc7f1719f797f627f8
paulauskas-linas <paulauskas.linas@gmail.com>
2022-03-31 07:27:30 +0300

    05 exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(...args) {
+    const argsArray = args.sort((a,b)=>a-b); //sorts array in numeric fashion, otherwise would sort by UTF-16 unit code value
+    let hasNegative = args.some(item => item < 0);
+    let isString = args.some(item => typeof item === "string");
+    let isArray = args.some(item => Array.isArray(item));
+    if (hasNegative === true || 
+        isString === true ||
+        isArray === true) {
+        return "ERROR"
+    } else {
+       let sum = 0;
+        while (argsArray[0] <= argsArray[1]) {
+            sum +=argsArray[0];
+            argsArray[0]++;
+        }
+        return sum; 
+    }
 };
 
 // Do not edit below this line
