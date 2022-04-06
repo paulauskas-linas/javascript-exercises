0597c84f962bf6f47024f19a9bfa23287840a7f0
paulauskas-linas <paulauskas.linas@gmail.com>
2022-04-06 07:45:48 +0300

    07 exercise done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+let c = (f - 32) * 5/9;
+let cr = Math.round(c * 10) / 10;
+return cr;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+let f = c * 9/5 + 32;
+let fr = Math.round(f * 10) / 10;
+return fr;
 };
 
 // Do not edit below this line
