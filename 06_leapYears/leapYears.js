afbb5edeb3eba8df9ae5ae10100fcc63a8ccbed5
paulauskas-linas <paulauskas.linas@gmail.com>
2022-04-06 07:30:34 +0300

    06 exercise done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+if (year % 400 == 0) {
+        return true;
+    } else if ((year % 4 != 0) || (year % 100 == 0)) {
+    return false;
+} else return true
 };
 
+//TOP solution 
+//return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
 // Do not edit below this line
 module.exports = leapYears;
