90e30ca352e8c655c019f00d82d82a2e8a9a2531
paulauskas-linas <paulauskas.linas@gmail.com>
2022-05-19 08:58:15 +0300

    09 exercise done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //take the string, make lowercase for case-sensitive removal and make an array of it
+    const editedArray = string.toLowerCase().split('');
+    // filter the array for punctuation
+    for (let i = 0; i < editedArray.length; i++) {
+        if(editedArray[i] === '!' ||
+        editedArray[i]  === ',' ||
+        editedArray[i]  === '.' || 
+        editedArray[i]  === '?' ||
+        editedArray[i]  === ' ') {
+            editedArray.splice(i, 1, '');
+        }
+    }
+    // make string from array
+    const editedString = editedArray.join('');
+    // make a copy of original array, reverse and join into the string
+    const reversedString = Array.from(editedArray).reverse().join('');
+    // compare the strings
+    return reversedString == editedString;
 
 };
 
