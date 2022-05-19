5cf2856bb94893a05fa41023e37df17717b1ffab
paulauskas-linas <paulauskas.linas@gmail.com>
2022-05-19 16:27:08 +0300

    12 exercise done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldestToYoungest = people.sort(function(a, b) {
+        const calcAge = function(dead, born){
+            if(!dead) {
+                dead = new Date().getFullYear();
+            }
+            return dead - born;
+        };
+        const lastPerson = calcAge(a.yearOfDeath, a.yearOfBirth);
+        const nextPerson = calcAge(b.yearOfDeath, b.yearOfBirth);
+        return lastPerson > nextPerson ? -1 : 1;
+    })
+   return oldestToYoungest[0];
 };
 
 // Do not edit below this line
